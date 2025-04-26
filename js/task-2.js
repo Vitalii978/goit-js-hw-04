669a0f4dcd720ebd8c0a238fb8989a6396a9c58e
Vitalii Klymenko <Vitalii.Klymenko78@gmail.com>
2025-04-26 09:58:47 +0200

    error correct

--- js/task-2.js
+++ js/task-2.js
@@ -1,4 +1,8 @@
 function calcAverageCalories(days) {
+  if (days.length === 0) {
+    return 0
+  }
+
     let totalCalories = 0;
 
     for (const day of days) {
@@ -6,6 +10,7 @@ function calcAverageCalories(days) {
     }
     
     return totalCalories / days.length;
+    
 }
 
 
